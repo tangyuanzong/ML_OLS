ac0fb7bfdbc2a184d22a612943558d4f91f31aa5
tyz <1796062210@qq.com>
2017-11-19 19:10:09 +0800

    Update data_1.py

--- data_1.py
+++ data_1.py
@@ -1,30 +1,30 @@
 #!/usr/bin/python
 #-*- coding: utf-8 -*-
 from numpy import *
-import matplotlib.pyplot as plt
-import numpy as np
-import time
+import matplotlib.pyplot as plt          #导入matplotlib包
+import numpy as np                       #导入numpy包
+import time                              
 
 def loadData():                           #读入文件
     f=open('ex1data1.txt')                      
-    data = f.readlines()                        
-    l=len(data)                                
-    mat=zeros((l,2))                             
-    index=0 
-    xdata = ones((l,2))
+    data = f.readlines()                  #一次读入文件     
+    l=len(data)                           
+    mat=zeros((l,2))                      #l*2的矩阵，初始化0       
+    index=0                               
+    xdata = ones((l,2))                   #l*2的矩阵，初始化为1
     ydata = []                       
     for line in data:
-        line = line.strip()                     
-        linedata = line.split(',')               
-        mat[index, :] = linedata[0:2]          
+        line = line.strip()               #取出多余字符，    
+        linedata = line.split(',')        #分割数据
+        mat[index, :] = linedata[0:2]     #暂存在mat矩阵中
         index +=1
-    xdata[:,1] = mat[:,0]
-    ydata = mat[:,1]
+    xdata[:,1] = mat[:,0]                 #将mat的第0列存入xdata的第一列
+    ydata = mat[:,1]                      #将mat的第1列存入ydata中
     return xdata,ydata
 
 def model(theta,x):                       #直线方程
     theta = np.array(theta)
-    return x.dot(theta)
+    return x.dot(theta)                   #返回直线的y值
 
 def cost(theta,xdata,ydata,l):            #代价函数
     SUM = 0
@@ -32,18 +32,18 @@ def cost(theta,xdata,ydata,l):            #代价函数
     ydata = mat (ydata)
     ydata = ydata.T
     for line in ydata:
-          yp = model(theta,xdata[idex ,:])
+          yp = model(theta,xdata[idex ,:]) 
           yp = yp - ydata[idex ,:]
           yp = yp**2
           SUM = SUM + yp
           idex  =idex +1
-    return SUM/2/l
+    return SUM/2/l                       #返回耗费
 
 def grad(theta,idex1,xdata,ydata,sigmal,l):  #梯度计算
     idex = 0
     SUM = 0
     for line in ydata:
-            yp = model(theta,xdata[idex ,:]) - ydata[idex]
+            yp = model(theta,xdata[idex ,:]) - ydata[idex] #计算梯度
             yp =yp * xdata[idex][idex1]
             idex  =idex +1
             SUM = SUM + yp
@@ -53,11 +53,11 @@ def grad(theta,idex1,xdata,ydata,sigmal,l):  #梯度计算
 def gradlient(theta,xdata,ydata,sigmal,l):  #参数更新
     idex1 = 0
     for line1 in theta:
-            theta[idex1] = theta[idex1] - sigmal * grad(theta,idex1,xdata,ydata,sigmal,l)
+            theta[idex1] = theta[idex1] - sigmal * grad(theta,idex1,xdata,ydata,sigmal,l) #更新参数
             theta[idex1] = theta[idex1]
             idex1 = idex1+1
     
-    return theta
+    return theta        #返回参数
 
 def OLS(xdata,ydata):                          #梯度下降求解
     start = time.clock()
@@ -69,34 +69,34 @@ def OLS(xdata,ydata):                          #梯度下降求解
     cost_record = []
     it = []
     sigmal = 0.01
-    cost_val = cost(theta,xdata,ydata,l)
+    cost_val = cost(theta,xdata,ydata,l)    #计算代价
     cost_record.append(cost_val)
     it.append(iters)
     while iters <1500:
-          theta = gradlient(theta,xdata,ydata,sigmal,l)
-          cost_updata = cost(theta,xdata,ydata,l)
+          theta = gradlient(theta,xdata,ydata,sigmal,l)  #更新参数
+          cost_updata = cost(theta,xdata,ydata,l)        #计算代价
           iters = iters + 1
-          cost_val = cost_updata
-          cost_record.append(cost_val)
-          it.append(iters)
+          cost_val = cost_updata         
+          cost_record.append(cost_val)                    #记录代价
+          it.append(iters)                                #记录迭代次数
     end = time.clock()
     return mat(theta).T,cost_record,it
 
-def show(xArr,yArr):
+def show(xArr,yArr):                             #显示回归直线
     plt.xlabel('Population',size=25)             # 横坐标
     plt.ylabel('Profit'    ,size=25)             # 纵坐标
     plt.title('Linear Regression',size=30)       # 标题
     xCopy =xArr.copy()  
     xCopy.sort(0)  
     yHat = xCopy * ws  
-    plt.plot(xArr[:,1],yArr,'go',label='y1')
+    plt.plot(xArr[:,1],yArr,'go',label='y1')   
     plt.plot(xCopy[:,1],yHat,label='y2')  
     plt.show()  
 
-def show1(xArr,yArr):
+def show1(xArr,yArr):                             #显示代价
     plt.xlabel('iters',size=25)                   # 横坐标
-    plt.ylabel('cost' ,size=25)              # 纵坐标
-    plt.title('Linear Regression',size=30)       # 标题
+    plt.ylabel('cost' ,size=25)                   # 纵坐标
+    plt.title('Linear Regression',size=30)        # 标题
     plt.plot(xArr[0:,],yArr[0:,],'go',label='y1')
     plt.show()
 
